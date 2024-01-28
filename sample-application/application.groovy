94203a467c7614ef5858dda650faeb2fc6c55e00
iamhritik <hritik.99@outlook.com>
2024-01-29 01:42:21 +0530

    testing with declarative pipeline

--- sample-application/application.groovy
+++ sample-application/application.groovy
@@ -26,22 +26,25 @@
 // }
 
 pipeline {
+    agent any
     stages {
         stage("testing shared library") {
-            script {
-                    try {
-                        def callLibraryTemplate = new poc.template.sample()
-                        callLibraryTemplate.call(
-                            repo_url: "https://github.com/iamhritik/poc-repo.git",
-                            repo_branch: "master",
-                            repo_creds: "github-creds",
-                            // message: "From shared library POC"
-                        )
-                    }
-                    catch (Exception e){
-                        currentBuild.result = "FAILURE"
-                        echo "Faced some issue in the pipeline"
-                    }
+            steps {
+                script {
+                        try {
+                            def callLibraryTemplate = new poc.template.sample()
+                            callLibraryTemplate.call(
+                                repo_url: "https://github.com/iamhritik/poc-repo.git",
+                                repo_branch: "master",
+                                repo_creds: "github-creds",
+                                // message: "From shared library POC"
+                            )
+                        }
+                        catch (Exception e){
+                            currentBuild.result = "FAILURE"
+                            echo "Faced some issue in the pipeline"
+                        }
+                }
             }
         }
     }
