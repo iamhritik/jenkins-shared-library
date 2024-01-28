5e4bdb1b4889d83fb3ad1dc14ed145d3667c38e9
iamhritik <hritik.99@outlook.com>
2024-01-29 01:40:11 +0530

    testing sub func calling

--- sample-application/application.groovy
+++ sample-application/application.groovy
@@ -1,17 +1,44 @@
 @Library('poc') _
 
-node {
-    try {
-        def callLibraryTemplate = new poc.template.sample()
-        callLibraryTemplate.call.git_checkout(
-            repo_url: "https://github.com/iamhritik/poc-repo.git",
-            repo_branch: "master",
-            repo_creds: "github-creds",
-            // message: "From shared library POC"
-        )
-    }
-    catch (Exception e){
-        currentBuild.result = "FAILURE"
-        echo "Faced some issue in the pipeline"
+// node {
+//     try {
+//         def callLibraryTemplate = new poc.template.sample()
+//         callLibraryTemplate.call(
+//             repo_url: "https://github.com/iamhritik/poc-repo.git",
+//             repo_branch: "master",
+//             repo_creds: "github-creds",
+//             // message: "From shared library POC"
+//         )
+//         def callLibraryTemplate = new poc.template.sample()
+//         callLibraryTemplate.call2(
+//             // repo_url: "https://github.com/iamhritik/poc-repo.git",
+//             // repo_branch: "master",
+//             // repo_creds: "github-creds",
+//             message: "From shared library POC"
+//         )
+
+//         call2
+//     }
+//     catch (Exception e){
+//         currentBuild.result = "FAILURE"
+//         echo "Faced some issue in the pipeline"
+//     }
+// }
+
+pipeline {
+    stages {
+        stage("testing shared library") {
+            script {
+                    try {
+                        def callLibraryTemplate = new poc.template.sample()
+                        callLibraryTemplate.call(
+                            repo_url: "https://github.com/iamhritik/poc-repo.git",
+                            repo_branch: "master",
+                            repo_creds: "github-creds",
+                            // message: "From shared library POC"
+                        )
+                    }
+            }
+        }
     }
 }
--- src/poc/template/sample.groovy
+++ src/poc/template/sample.groovy
@@ -5,13 +5,29 @@ import poc.main.*
 
 def call(Map stepParams) {
     git_checkout = new checkout()
-    printMessage = new helloworld()
+    // printMessage = new helloworld()
 
     git_checkout.gitCheckout(
         repo_url: "${stepParams.repo_url}",
         repo_branch: "${stepParams.repo_branch}",
         repo_creds: "${stepParams.repo_creds}"
     )
+
+    // printMessage.helloWorldMessage(
+    //     message: "${stepParams.message}"
+    // )
+}
+
+
+def call2(Map stepParams) {
+    // git_checkout = new checkout()
+    printMessage = new helloworld()
+
+    // git_checkout.gitCheckout(
+    //     repo_url: "${stepParams.repo_url}",
+    //     repo_branch: "${stepParams.repo_branch}",
+    //     repo_creds: "${stepParams.repo_creds}"
+    // )
 
     printMessage.helloWorldMessage(
         message: "${stepParams.message}"
