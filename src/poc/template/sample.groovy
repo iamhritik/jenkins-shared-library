1173196547de87882b86d50f57f1e0594c698c08
iamhritik <hritik.99@outlook.com>
2024-01-29 01:22:19 +0530

    fix template block in library code

--- src/poc/template/sample.groovy
+++ src/poc/template/sample.groovy
@@ -6,14 +6,14 @@ package poc.main.*
 def call(Map stepParams){
     git_checkout = new checkout()
     printMessage = new helloworld()
+
+    git_checkout.gitCheckout(
+        repo_url: "${stepParams.repo_url}"
+        repo_branch: "${stepParams.repo_branch}"
+        repo_creds: "${stepParams.repo_creds}"
+    )
+
+    printMessage.helloWorldMessage(
+        message: "${stepParams.message}"
+    )
 }
-
-git_checkout.gitCheckout(
-    repo_url: "${stepParams.repo_url}"
-    repo_branch: "${stepParams.repo_branch}"
-    repo_creds: "${stepParams.repo_creds}"
-)
-
-printMessage.helloWorldMessage(
-    message: "${stepParams.message}"
-)
